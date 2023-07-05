6165fb08a3dd2c5b8fd0fb6c2c7c884c2e6216a4
Ubuntu <ubuntu@ip-172-31-60-194.ec2.internal>
2023-07-05 04:16:09 +0000

    local sprint boot project

---
+++ src/main/java/com/damondeng/spapp/SpappApplication.java
@@ -0,0 +1,13 @@
+package com.damondeng.spapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damondeng/spapp/SpappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damondeng.spapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
