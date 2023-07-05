39999c76069b877ab7d616ef4387c96a9fc549db
Deng <mingxuan@bcd0740dbeab.ant.amazon.com>
2023-07-05 17:23:21 +0800

    fixing some package issue

--- src/main/java/com/damondeng/spapp/service/EmployeeService.java
+++ src/main/java/com/damondeng/spapp/service/EmployeeService.java
@@ -2,6 +2,9 @@ package com.damondeng.spapp.service;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
+import com.damondeng.spapp.repository.EmployeeRepository;
+import com.damondeng.spapp.model.Employee;
+
 
 @Service
 public class EmployeeService {
